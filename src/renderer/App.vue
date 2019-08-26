e1124db527f57ff5f44162d9110ec1bc382b3eed
taoyanhong <1849252806@qq.com>
2019-08-26 18:46:54 +0800

    初始化

---
+++ src/renderer/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'listen'
+  }
+</script>
+
+<style lang="scss">
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+</style>
---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="listen">
+    <div class="layout_head">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data: () => {
+      return {
+      }
+    },
+    created: () => {
+    }
+  }
+</script>
+
+<style lang="scss">
+  #listen {
+    .layout_head {
+      height: 50px;
+      background: #C62F2F;
+      -webkit-app-region: drag;
+    }
+  }
+
+</style>
