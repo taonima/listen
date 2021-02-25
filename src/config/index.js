d824581cfe51d4f55bc8d5dd2f9cffc80e3169c9
taoyanhong <1849252806@qq.com>
2021-02-25 11:11:19 +0800

    存档

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const baseUrl = process.env.NODE_ENV !== 'production'
-  ? 'http://localhost:3000'
-  : 'http://localhost:3000'
+  ? 'http://localhost:3333'
+  : 'http://localhost:3333'
 
 const winURL = process.env.NODE_ENV !== 'production'
   ? 'http://localhost:9020/#/'
